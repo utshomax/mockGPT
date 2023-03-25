498707a4f760832d25b8c00530df5ccf29479eb2
utsab <utsab8008@gmail.com>
2023-03-25 19:09:19 +0530

    Added Express

--- src/index.ts
+++ src/index.ts
@@ -1,47 +1,15 @@
-import * as dotenv from 'dotenv' 
-import { addGPTContext, getGPTContext, setGPTContext, TGPTConversation } from './context';
-dotenv.config()
-import {openai } from "./services/openai/config";
-const readline = require('readline');
+import express, { Express, Request, Response } from 'express';
+import dotenv from 'dotenv';
 
-const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
-const prompt = (query:any) => new Promise((resolve) => rl.question(query, resolve))
+dotenv.config();
 
-type TCompletionMessage = TGPTConversation[]
-async function getCompletion(context: TCompletionMessage | undefined) {
-    if(!context) return
-    console.log(context)
-    const response = await openai.createChatCompletion({
-          model: "gpt-3.5-turbo",
-          messages: context,
-    });
-    console.log(response.data)
-    return response.data.choices[0].message?.content
+const app: Express = express();
+const port = process.env.PORT;
 
-}
+app.get('/', (req: Request, res: Response) => {
+  res.send('Express + TypeScript Server');
+});
 
-async function  main() {
-    let userContext =  setGPTContext("test")
-    //Get input from user
-    while(true){
-    let content =await prompt("User : ") as string
-    let newMessage: TGPTConversation = {
-        role: "user",
-        content,
-    }
-    if (userContext !== undefined) {
-        userContext = addGPTContext("test", newMessage)
-        let completion = await getCompletion(userContext)
-        if(completion){
-            addGPTContext('test',{
-                role: 'assistant',
-                content: completion
-            })
-        }
-        console.log(completion)
-    }
-}
-    //console.log(await getCompletion("This is a test"))
-}
-
-main()
+app.listen(port, () => {
+  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+});
